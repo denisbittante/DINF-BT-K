2558cfff2c82b7b0da84025173da8d981a5966f2
Denis Bittante <denisbittante@gmail.com>
2018-03-06 02:55:21 +0100

    Scheduler-Utility beschrieben

--- 4_Evaluation/Log_Extractor/src/ch/ffhs/dinf/utils/jmx/time/Main.java
+++ 4_Evaluation/Log_Extractor/src/ch/ffhs/dinf/utils/jmx/time/Main.java
@@ -3,18 +3,23 @@ package ch.ffhs.dinf.utils.jmx.time;
 import java.util.GregorianCalendar;
 
 import ch.ffhs.dinf.utils.FileUtils;
-
+/**
+* Genutzt um die Tests zeitlich neu einzustellen. 
+*
+*/
 public class Main {
 
-	private static final String outputFilename = "C:\\sandbox\\dinf\\dinf\\loadtest\\jmeter\\LoadTest-TIMED.jmx";
 
 	private static final String outputBackupFilename = "C:\\sandbox\\dinf\\dinf\\loadtest\\jmeter\\backup\\LoadTest-Backup"
 			+ new GregorianCalendar().getTimeInMillis() + ".jmx";
 
 	private static final String inputFilename = "C:\\sandbox\\dinf\\dinf\\loadtest\\jmeter\\DinfLoadTest.jmx";
+	private static final String outputFilename = "C:\\sandbox\\dinf\\dinf\\loadtest\\jmeter\\LoadTest-TIMED.jmx";
 
 	private static final GregorianCalendar startTimeStamp = new GregorianCalendar(2018, GregorianCalendar.FEBRUARY, 9,
 			01, 00, 00);
+			
+	// Stellt 10 Minuten Laufzeit und 10 Minuten Cool-Down Phase ein, von einem gegebenen Zeitpunkt an (startTimeStamp)	
 	private static final double runningTime = 10d * 60d * 1000d;
 	private static final double breakTime = 10d * 60d * 1000d;
 
@@ -23,7 +28,6 @@ public class Main {
 		String readFile = FileUtils.readFile(inputFilename);
 		SchedulerChanger changer = new SchedulerChanger(runningTime, breakTime);
 		String findAndReplaceSchedule = changer.findAndReplaceSchedule(readFile, startTimeStamp);
-		// System.out.println(findAndReplaceSchedule);
 
 		FileUtils.writeFile(findAndReplaceSchedule, outputFilename);
 		FileUtils.writeFile(findAndReplaceSchedule, outputBackupFilename);
